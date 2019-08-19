5e77dee7ffbbd64258fb15fefe4067b3feda5bd7
Jryke <rykerson@gmail.com>
2019-08-19 14:50:31 +0700

    020502

--- classes.js
+++ classes.js
@@ -12,3 +12,24 @@ let buildCar = (make, model, color) => {return new Car(make, model, color)}
 let myCar = buildCar('Chevrolet', 'Bel-Air', 'Tropical Turquoise')
 
 console.log(myCar)
+
+// 020502
+let names = ['Alice', 'Bernard', 'Catherine', 'David']
+let jobs = ['Accountant', 'Biz Manager', 'Cleaner', 'Director']
+
+let Employee = class {
+	constructor (name, job) {
+		this.name = name
+		this.job = job
+	}
+}
+
+let makeEmployees = (arr1, arr2) => {
+	let employeesArr = []
+	arr1.forEach((name, i) => {
+		employeesArr.push(new Employee(name, arr2[i]))
+	})
+	return employeesArr
+}
+
+console.log(makeEmployees(names, jobs))
