9e895b581e70bedecfbde1788de3608015b7be75
Jryke <rykerson@gmail.com>
2019-08-19 14:33:05 +0700

    020402

--- time.js
+++ time.js
@@ -6,3 +6,32 @@ const b = (func, num) => {
 
 b(a, 3)
 b(a, 5)
+
+// 020402
+let makeHelloInterval = () => {
+	let seconds = 0
+
+	let helloInterval = setInterval(() => {
+		seconds++
+		console.log('hello ' + seconds)
+		if (seconds == 5) {
+			clearInterval(helloInterval)
+		}
+	}
+	, 1000)
+}
+
+makeHelloInterval()
+
+
+// 020402 (alternative solution)
+let makeHelloInterval2 = () => {
+	setTimeout(() => clearInterval(interval), 6000)
+	let seconds = 0
+	let interval = setInterval(() => {
+		seconds++
+		console.log('Hello ' + seconds)}
+		, 1000)
+}
+
+// makeHelloInterval2()
